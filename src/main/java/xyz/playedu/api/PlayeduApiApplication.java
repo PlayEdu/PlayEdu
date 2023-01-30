e686340ce6347c0a9769c19c7e775df88f68b50c
none <none>
2023-01-30 15:32:04 +0800

    project init

---
+++ src/main/java/xyz/playedu/api/PlayeduApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.playedu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayeduApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayeduApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/playedu/api/PlayeduApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.playedu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayeduApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
