e1796e423bbc7f99eb014ef3ba6034912adc34f2
none <none>
2023-02-23 10:31:56 +0800

    删除无用代码

--- src/main/java/xyz/playedu/api/domain/AdminRolePermission.java
+++ src/main/java/xyz/playedu/api/domain/AdminRolePermission.java
@@ -1,8 +1,6 @@
 package xyz.playedu.api.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import lombok.Data;
