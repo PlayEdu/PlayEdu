7702d92dc8fc0c27b18bb02bd900ce43fe9d2ce3
none <none>
2023-03-24 17:44:22 +0800

    added: api文档

---
+++ src/main/java/xyz/playedu/api/controller/frontend/IndexController.java
@@ -0,0 +1,17 @@
+package xyz.playedu.api.controller.frontend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.playedu.api.types.JsonResponse;
+
+/**
+ * @Author 杭州白书科技有限公司
+ * @create 2023/3/24 17:42
+ */
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public JsonResponse index() {
+        return JsonResponse.success();
+    }
+}
