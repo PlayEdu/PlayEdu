b403cbf2b509260751b0f252c953d7acad03fc66
none <none>
2023-04-17 15:09:02 +0800

    license-header修改

--- src/main/java/xyz/playedu/api/service/internal/UserDepartmentService.java
+++ src/main/java/xyz/playedu/api/service/internal/UserDepartmentService.java
@@ -1,6 +1,17 @@
-/**
- * This file is part of the PlayEdu.
- * (c) 杭州白书科技有限公司
+/*
+ * Copyright 2023 杭州白书科技有限公司
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package xyz.playedu.api.service.internal;
 
