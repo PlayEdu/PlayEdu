4c085d48363a717124bdf5d75a1165b6c17f88bc
none <none>
2023-07-03 17:28:45 +0800

    fixed: 前后台的账号体系的冲突

--- src/main/java/xyz/playedu/api/service/impl/BackendAuthServiceImpl.java
+++ src/main/java/xyz/playedu/api/service/impl/BackendAuthServiceImpl.java
@@ -26,11 +26,12 @@ import java.util.HashMap;
 
 @Service
 public class BackendAuthServiceImpl implements BackendAuthService {
-    @Autowired private AuthService authService;
+    @Autowired
+    private AuthService authService;
 
     @Override
     public String loginUsingId(Integer userId, String loginUrl) {
-        return authService.loginUsingId(userId, loginUrl, SystemConstant.JWT_PRV_ADMIN_USER);
+        return authService.loginUsingId(100000000 + userId, loginUrl, SystemConstant.JWT_PRV_ADMIN_USER);
     }
 
     @Override
@@ -40,7 +41,7 @@ public class BackendAuthServiceImpl implements BackendAuthService {
 
     @Override
     public Integer userId() {
-        return authService.userId();
+        return authService.userId() - 100000000;
     }
 
     @Override
